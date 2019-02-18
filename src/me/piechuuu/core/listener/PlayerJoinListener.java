eb277bd9d7f422cbfe26e08c724e840356db65a3
Piechuuu <RoDaSekYT@wp.pl>
2019-02-18 19:47:53 +0100

    JoinPlayerMessage

--- src/me/piechuuu/core/listener/PlayerJoinListener.java
+++ src/me/piechuuu/core/listener/PlayerJoinListener.java
@@ -1,6 +1,8 @@
 package me.piechuuu.core.listener;
 
+import com.connorlinfoot.titleapi.TitleAPI;
 import me.piechuuu.core.managers.FileManager;
+import me.piechuuu.core.vanish.utils.ChatUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,7 +14,10 @@ public class PlayerJoinListener implements Listener {
     public void joinEvent(PlayerJoinEvent e) {
         Player p = e.getPlayer();
         FileManager.createPlayerFile(p);
-
+        for (int i = 0; i < 100; ++i) ;
+        p.sendMessage(ChatUtil.fixColor("&7Witaj na serwerze &cBAPE&4HC.PL"));
+        p.sendMessage(ChatUtil.fixColor("&7Znajdujesz sie &cMedium&4HC"));
+        p.sendMessage(ChatUtil.fixColor("&7Zyczymy ci milesj gry :)"));
+        TitleAPI.sendFullTitle(p,20,50,20,"§cBAPE&4HC.PL", "§8» §7Wiecej pod §c/pomoc :)");
     }
-
 }

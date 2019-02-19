25da65cbe98196fd0aaa25936a6cb26cd6c5b31f
Piechuuu <RoDaSekYT@wp.pl>
2019-02-19 14:55:58 +0100

    vanish

--- src/me/piechuuu/core/commands/VanishCommand.java
+++ src/me/piechuuu/core/commands/VanishCommand.java
@@ -12,16 +12,16 @@ public class VanishCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (!(commandSender instanceof Player)) return ChatUtil.sendMessage(commandSender, "&8>> &cTej komendy nie mozna uzyc z poziomu konsoli!");
+        if (!(commandSender instanceof Player)) return ChatUtil.sendMessage(commandSender, "§8>> &cTej komendy nie mozna uzyc z poziomu konsoli!");
         if(!commandSender.hasPermission("bapehc.core.vanish")){
-            commandSender.sendMessage(ChatUtil.fixColor("&4Blad: &cNie masz dostepu do &7(bapehc.core.vanish)"));
+            commandSender.sendMessage(ChatUtil.fixColor("§4Blad: §cNie masz dostepu do §7(bapehc.core.vanish)"));
             return false;
         }
         final Player player = (Player) commandSender;
         final Boolean b = VanishManager.isVanish(player);
         if (!b) VanishManager.addPlayer(player);
         else VanishManager.removePlayer(player);
-        Utils.vanish(player, b, "&8>> &cAdministrator &7"+player.getName()+" &7jest: &7"+ (b ? "&aWIDZIALNY" : "&cNIE WIDZIALNY"));
+        Utils.vanish(player, b, "§8>> §cAdministrator &7"+player.getName()+" §7jest: &7"+ (b ? "§aWIDZIALNY" : "§cNIE WIDZIALNY"));
         return false;
     }
 }

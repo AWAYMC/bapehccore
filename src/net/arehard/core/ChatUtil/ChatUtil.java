87d7413bf2dbcec0d7d76fb48256d0f68420d331
adrianekk88 <48407335+adrianekk88@users.noreply.github.com>
2019-03-17 14:21:07 +0100

    uuuu

--- src/net/arehard/core/ChatUtil/ChatUtil.java
+++ src/net/arehard/core/ChatUtil/ChatUtil.java
@@ -1,12 +1,16 @@
 package net.arehard.core.ChatUtil;
 
-import org.bukkit.ChatColor;
+import org.bukkit.*;
+import org.bukkit.entity.Player;
 
 public class ChatUtil {
 
-    public static String fixColor(String msg) {
-        if (msg == null) return "";
-        return ChatColor.translateAlternateColorCodes('&', msg);
+    public static String fixColor(String text) {
+        return ChatColor.translateAlternateColorCodes('&', text.replace(">>", "»").replace("<<", "«"));
+    }
+
+    public static void sendMessage(Player p, String text) {
+        p.sendMessage(fixColor(text));
     }
 }
 

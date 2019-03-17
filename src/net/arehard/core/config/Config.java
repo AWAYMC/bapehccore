7fd97a063cc782f85f5631b17a3b10155ecad8dd
adrianekk88 <48407335+adrianekk88@users.noreply.github.com>
2019-03-17 17:34:23 +0100

    asfdasdfas

--- src/net/arehard/core/config/Config.java
+++ src/net/arehard/core/config/Config.java
@@ -13,8 +13,8 @@ public class Config
     public static List<String> SETTINGS_SPONSOR;
 
     static {
-        Config.SETTINGS_VIP = Arrays.asList("test", "test", "test", "test","test","test","test","test","test");
-        Config.SETTINGS_SPONSOR = Arrays.asList("test", "test", "test", "test","test","test","test","test","test");
+        Config.SETTINGS_VIP = Arrays.asList("test", "test", "test", "test", "test", "test", "test", "test", "test");
+        Config.SETTINGS_SPONSOR = Arrays.asList("test", "test", "test", "test", "test", "test", "test", "test", "test");
     }
 
     public static void loadConfig() {
